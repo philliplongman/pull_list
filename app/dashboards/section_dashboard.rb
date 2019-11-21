60778725dd8b7555c87c300a77436de50e157e52
Phillip Longman <himself@philliplongman.com>
2019-11-20 20:00:03 -0500

    Generate Administrate install.

---
+++ app/dashboards/section_dashboard.rb
@@ -0,0 +1,72 @@
+require "administrate/base_dashboard"
+
+class SectionDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id:                 Field::Number,
+    name:               Field::String,
+    created_at:         Field::DateTime,
+    updated_at:         Field::DateTime,
+    area:               Field::BelongsTo,
+    sections_products:  Field::HasMany,
+    products:           Field::HasMany,
+    sections_sizes:     Field::HasMany,
+    sizes:              Field::HasMany
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    name
+    area
+    sizes
+    products
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    name
+    created_at
+    updated_at
+    area
+    sizes
+    products
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    name
+    area
+    sizes
+    products
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how sections are displayed
+  # across all pages of the admin dashboard.
+  def display_resource(section)
+    section.name
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :managers
+    resources :areas
+    resources :brands
+    resources :products
+    resources :sections
+    resources :sizes
+
+    root to: "managers#index"
+  end
+
   devise_for :managers
 
   root "homes#index"
