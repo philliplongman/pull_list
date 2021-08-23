6552144dcf7a53c63337f576586cebb8afe1a1df
Phillip Longman <himself@philliplongman.com>
2021-08-23 18:32:10 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+say "Seeding database"
+
+managers = [
+  { name: "Dennis Lenaios", email: "winedad@gmail.com" },
+  { name: "Brigid Brewer",  email: "bri.from.kildare@gmail.com" },
+  { name: "Jen Sloe",       email: "jenever.sloe@gmail.com" }
+]
+
+managers.each do |info|
+  Manager.find_or_create_by!(info) do |manager|
+    manager.password = "password"
+  end
+end
+
+placements_data = Rails.root.join("db/seed_data.csv")
+
+CSV.foreach(placements_data, headers: true) do |row|
+  section = Section.find_or_create_by!(name: row["section.name"])
+  area    = Area.find_or_create_by!(name: row["area.name"], section: section)
+  brand   = Brand.find_or_create_by!(name: row["brand.name"])
+
+  product = Product.find_or_create_by!(
+    brand:   brand,
+    name:    row["product.name"],
+    variety: row["product.variety"]
+  )
+
+  size      = Size.find_or_create_by!(description: row["placement.size"])
+  container = Container.find_or_create_by!(product: product, size: size)
+
+  Placement.find_or_create_by!(area: area, container: container)
+end
