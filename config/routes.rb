d8f16fb472627dcce5a4af498a3fb1ab910daf29
Phillip Longman <himself@philliplongman.com>
2021-08-23 20:14:03 -0400

    Run administrate:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :managers
+    resources :areas
+    resources :brands
+    resources :placements
+    resources :products
+    resources :containers
+    resources :sizes
+    resources :sections
+
+    root to: "managers#index"
+  end
+
   devise_for :managers
 
   root to: "home#index"
